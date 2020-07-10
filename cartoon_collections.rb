f57bb3c3e5732bf5dbeef67457043874a20c0131
Cody C <codyicastro@gmail.com>
2020-07-10 20:41:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,41 @@
+require "pry"
+
 def greet_characters(array)
+  count = 0
+  array.each do |greet|
+    puts "Hello #{greet}!"
+  end
+  count += 1
+end
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
-end
+
+
+
+# def list_dwarves(array)
+# index = 1
+#   array.each_with_index do |list|
+#     while index < array.length
+#       print "#{index} #{list}!"
+#     end
+#   index += 1
+# end
+# end
+
 
 def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
-end
+  array.each do |list|
+    list_count = 1
+    index_count = 0
+    print "#{list_count}. #{array}"
+    binding.pry
+  index_count += 1
+  end
+  
+  end
+
+  
+
+  
